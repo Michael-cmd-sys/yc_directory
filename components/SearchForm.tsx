3854d9932975b050d48e1c3b01142483b1f0c47b
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-26 13:59:41 +0000

    feat:implement Hero page search functionality  && refactor: reorganize project structure and add shadcn button component

---
+++ components/SearchForm.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Form from 'next/form';
+import { Search } from 'lucide-react';
+import SearchFormReset from './SearchFormReset';
+
+const SearchForm = ({query}: {query? : string}) => {
+  return (
+    <Form action="/" className="search-form" scroll={false}>
+        <input
+            name="query"
+            defaultValue={query}
+            className="search-button"
+            placeholder="Search Startups"  
+            />
+
+            <div className="flex gap-2">
+                {query && (
+                    <SearchFormReset />
+                )}
+
+                <button type="submit" className="text-white search-btn">
+                    <Search className="size-5"/>
+                </button>
+            </div>
+    </Form>
+  )
+}
+
+export default SearchForm
