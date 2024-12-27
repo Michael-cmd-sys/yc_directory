c52cf0a44dc406f8c983abb45bf4e1ef217a6d4a
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2024-12-27 22:48:54 +0000

    refactor: minor bug fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,18 @@ import SearchForm from "../../components/SearchForm";
 export default async function Home({searchParams} : {
   searchParams: Promise<{ query?: string }>
 }) {
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 55,
+      author: { _id: 1},
+      _id: 1,
+      title: "",
+      description: "",
+      img: "",
+      category: ""
+    }
+  ]
   const query = (await searchParams).query
 
   return (
@@ -16,6 +28,19 @@ export default async function Home({searchParams} : {
       <SearchForm query={query}/>
     </section>
 
+;   <section className="section_container">
+      <p className="text-30-semibold">
+        {query? `Showing results for "${query}"`: "All Startups"}
+      </p>
+
+      <ul className="mt-5 card_grid">
+        { posts?.length > 0 ? (
+          posts.map((post: StartupCardType, index: number) => <StartupCard />)
+        ):(
+          <p className="no-results">No Startups found...</p>
+        )}
+      </ul>
+</section>
     </>
   );
 }
